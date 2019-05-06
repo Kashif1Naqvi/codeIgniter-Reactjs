80fcbab7c0d55fd016d532bbce5e957409f65757
masivuye cokile <masivuyecokile@gmail.com>
2019-05-06 16:55:01 +0200

    Update autoload.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 $autoload['packages'] = array();
-$autoload['libraries'] = array('database','table','form_validation');
+$autoload['libraries'] = array('database','table','form_validation','session');
 $autoload['drivers'] = array();
 $autoload['helper'] = array('url','file','text','form');
 $autoload['config'] = array();
